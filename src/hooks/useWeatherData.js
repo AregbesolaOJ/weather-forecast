6091d0ab984552ce7e050661d4efbbce7bc83b5c
Aregbesola John <aregbesolajohn@gmail.com>
2022-07-02 01:20:02 +0100

    chore: remove logs and keys

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -1,8 +1,8 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-const API_Key = 'e33e922f84msh2c5450e84e2b393p1d5f26jsn03c4b9d84e85';
-const API_Host = 'community-open-weather-map.p.rapidapi.com';
+const API_Key = '';
+const API_Host = '';
 
 export function useWeatherData(location) {
   const [loading, setLoading] = useState(false);
